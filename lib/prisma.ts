2ec71b11d4e77b2ffe3323ed1cdf0d83175a0c44
kv394 <kv394@nyu.edu>
2026-04-05 23:22:26 -0500

    fix: revert to standard Prisma engine for native Vercel IPv6 support

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,24 +1,11 @@
 import { PrismaClient } from '@prisma/client';
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
 
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-function createPrismaClient() {
-  const connectionString = process.env.DATABASE_URL;
-  if (!connectionString) {
-    throw new Error('DATABASE_URL is not set');
-  }
-
-  const pool = new Pool({ connectionString });
-  const adapter = new PrismaPg(pool);
-  return new PrismaClient({ adapter });
-}
-
-export const prisma = global.prisma || createPrismaClient();
+export const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
   global.prisma = prisma;
