8a500d6ef34d9a287f3ced4f1941863f1937e3dc
kv394 <kv394@nyu.edu>
2026-04-11 17:17:46 -0500

    UX

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        'xs': ['1.125rem', { lineHeight: '1.5rem' }],     // 18px (was 12px)
+        'sm': ['1.3125rem', { lineHeight: '1.875rem' }],  // 21px (was 14px)
+        'base': ['1.5rem', { lineHeight: '2.25rem' }],    // 24px (was 16px)
+        'lg': ['1.6875rem', { lineHeight: '2.625rem' }],  // 27px (was 18px)
+        'xl': ['1.875rem', { lineHeight: '2.625rem' }],   // 30px (was 20px)
+        '2xl': ['2.25rem', { lineHeight: '3rem' }],       // 36px (was 24px)
+        '3xl': ['2.8125rem', { lineHeight: '3.375rem' }], // 45px (was 30px)
+        '4xl': ['3.375rem', { lineHeight: '3.75rem' }],   // 54px (was 36px)
+        '5xl': ['4.5rem', { lineHeight: '1' }],           // 72px (was 48px)
+        '6xl': ['5.625rem', { lineHeight: '1' }],         // 90px (was 60px)
+        '7xl': ['6.75rem', { lineHeight: '1' }],          // 108px (was 72px)
+        '8xl': ['9rem', { lineHeight: '1' }],             // 144px (was 96px)
+        '9xl': ['12rem', { lineHeight: '1' }],            // 192px (was 128px)
+      },
       fontFamily: {
         sans: ['var(--font-sans)', 'sans-serif'],
         serif: ['var(--font-serif)', 'serif'],
