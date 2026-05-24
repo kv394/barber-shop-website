554420df7e51b22fb9df4da334f3ab67587d27cc
kv394 <kv394@nyu.edu>
2026-05-24 00:35:56 -0500

    fix: register processHourlyNotifications in Inngest serve handler

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,12 +1,13 @@
 import { serve } from 'inngest/next';
 import { inngest } from '@/inngest/client';
-import { processDailyTasks, generateHourlyUsageReports } from '@/inngest/functions';
+import { processDailyTasks, processHourlyNotifications, generateHourlyUsageReports } from '@/inngest/functions';
 
 // Create an API that serves zero-downtime background jobs and crons
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     processDailyTasks,
+    processHourlyNotifications,
     generateHourlyUsageReports,
   ],
 });
